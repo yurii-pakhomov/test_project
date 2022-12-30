34360af4cb0a3b961df5148b1313b53f532b89d9
yurii.pakhomov <yurii.pakhomov@gmail.com>
2022-12-30 16:30:02 +0200

    added some fixes

--- src/test/java/com/ipakhomov/pages/SearchResultPage.java
+++ src/test/java/com/ipakhomov/pages/SearchResultPage.java
@@ -2,6 +2,7 @@ package com.ipakhomov.pages;
 
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -17,6 +18,7 @@ public class SearchResultPage {
     private By resultAddress = By.xpath("//span[@data-testid='address']");
     private By checkInDateValue = By.xpath("//button[@data-testid='date-display-field-start']");
     private By checkOutDateValue = By.xpath("//button[@data-testid='date-display-field-end']");
+    private By dateFieldIcon = By.xpath("//div[@data-testid='date-display-field-date-in-icon']");
 
     public void allResultsHaveCorrectCityName(String cityName) {
         List<WebElement> addresses = driver.findElements(resultAddress);
@@ -28,10 +30,25 @@ public class SearchResultPage {
     public void checkInAndCheckOutDatesAreCorrectOnSearchResultPage(LocalDate expectedCheckInDate, LocalDate expectedCheckOutDate) {
         String checkInDateValue = driver.findElement(this.checkInDateValue).getText().toLowerCase();
         String checkOutDateValue = driver.findElement(this.checkOutDateValue).getText().toLowerCase();
-        assertThat(checkInDateValue).contains(String.valueOf(expectedCheckInDate.getYear()),
-                joinMonthAndDayNumber(expectedCheckInDate.getMonth().name().toLowerCase(), expectedCheckInDate.getDayOfMonth()));
-        assertThat(checkOutDateValue).contains(String.valueOf(expectedCheckOutDate.getYear()),
-                joinMonthAndDayNumber(expectedCheckOutDate.getMonth().name().toLowerCase(), expectedCheckOutDate.getDayOfMonth()));
+        String expectedCheckInMonthName = expectedCheckInDate.getMonth().name().toLowerCase();
+        String expectedCheckOutMonthName = expectedCheckOutDate.getMonth().name().toLowerCase();
+        if (isDateFieldIconDisplayed()) {
+            assertThat(checkInDateValue).contains(String.valueOf(expectedCheckInDate.getYear()),
+                    joinMonthAndDayNumber(expectedCheckInMonthName, expectedCheckInDate.getDayOfMonth()));
+            assertThat(checkOutDateValue).contains(String.valueOf(expectedCheckOutDate.getYear()),
+                    joinMonthAndDayNumber(expectedCheckOutMonthName, expectedCheckOutDate.getDayOfMonth()));
+        } else {
+            assertThat(checkInDateValue).contains(joinMonthAndDayNumber(expectedCheckInMonthName.substring(0, 3), expectedCheckInDate.getDayOfMonth()));
+            assertThat(checkOutDateValue).contains(joinMonthAndDayNumber(expectedCheckOutMonthName.substring(0, 3), expectedCheckOutDate.getDayOfMonth()));
+        }
+    }
+
+    public boolean isDateFieldIconDisplayed() {
+        try {
+            return driver.findElement(dateFieldIcon).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     private String joinMonthAndDayNumber(String monthName, int dayOfMonth) {
